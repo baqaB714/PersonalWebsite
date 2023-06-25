42037a9252fdf6efdc9560a3d1b9fdee9882b9d4
baqaB714 <baqa4731@gmail.com>
2023-06-25 23:26:11 +0330

    full edit 14020404-2

--- scripts/main.js
+++ scripts/main.js
@@ -4,16 +4,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   for (const elm of document.getElementsByClassName('pic_doc')) {
-    
+
     elm.addEventListener('mouseover', (e) => {
-        e.target.classList.remove('scale_min')
+      e.target.classList.remove('scale_min')
     }
     )
   }
   for (const elm of document.getElementsByClassName('pic_doc')) {
-    
+
     elm.addEventListener('mouseleave', (e) => {
-        e.target.classList.add('scale_min')
+      e.target.classList.add('scale_min')
     }
     )
   }
@@ -24,8 +24,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-$(document).ready(function() {
-  AOS.init( {
+$(document).ready(function () {
+  AOS.init({
     // uncomment below for on-scroll animations to played only once
     // once: true  
   }); // initialize animate on scroll library
@@ -33,34 +33,34 @@ $(document).ready(function() {
 
 // Smooth scroll for links with hashes
 $('a.smooth-scroll')
-.click(function(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
     }
-  }
-});
+  });
